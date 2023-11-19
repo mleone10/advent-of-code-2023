1ce98034fa0f7b94cf910a74edeb10ff3255006a
mleone10 <mleone10@gmail.com>
2023-11-18 22:03:33 -0500

    Memoize maths.Factors, simplify 2015.20.1 present calculation

--- days/2015day20/day20.go
+++ days/2015day20/day20.go
@@ -1,6 +1,8 @@
 package day20
 
-import "github.com/mleone10/advent-of-code-2023/internal/maths"
+import (
+	"github.com/mleone10/advent-of-code-2023/internal/maths"
+)
 
 // Today's puzzle seems to be all about prime numbers.  Specifically, the elves that visit a house are those whose numbers are the prime factors of the given house's number.
 
@@ -18,8 +20,8 @@ func CalcPresents(house int) int {
 	p := 0
 	// TODO: refactor this to use a slice.Reduce function
 	for _, f := range fs {
-		p += f * 10
+		p += f
 	}
 
-	return p
+	return p * 10
 }
--- internal/maths/maths.go
+++ internal/maths/maths.go
@@ -2,6 +2,8 @@ package maths
 
 import "github.com/mleone10/advent-of-code-2023/internal/set"
 
+var memoFactors = map[int][]int{}
+
 // Factors calculates all factors of `n`, returning them in a slice of ints.
 func Factors(n int) []int {
 	fs := set.Set[int]{}
@@ -11,11 +13,17 @@ func Factors(n int) []int {
 		return []int{1}
 	}
 
+	// Check for cached factors in memo
+	if f, ok := memoFactors[n]; ok {
+		return f
+	}
+
 	// Loop through integers.  When a factor is found, store it, then recurse to find all Factors of the quotient.  Add them to the map.
 	for i := 2; i <= n; i++ {
 		if n%i == 0 {
 			fs.Add(i)
 			fs.Add(Factors(n / i)...)
+			memoFactors[n] = append(memoFactors[n], fs.Slice()...)
 		}
 	}
 
